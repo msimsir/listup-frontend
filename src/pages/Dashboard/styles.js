ad3ff25763f992480b5f2a48c737bec32b4968ef
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-03-03 02:00:01 +0300

    updated dashboard sidebar styling

--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -4,7 +4,7 @@ export const DashboardContainer = styled.div`
   width: 100%;
   height: 100vh;
   display: grid;
-  grid-template-columns: 0.8fr 2fr 1.2fr;
+  grid-template-columns: minmax(300px, 0.8fr) 2fr 1.2fr;
   grid-template-rows: 0.3fr 1.5fr 1.5fr;
   grid-template-areas:
     "sidebar main details"
