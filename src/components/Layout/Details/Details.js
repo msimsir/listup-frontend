d3b7af215dcf0137c82df50114b54f7e78cb449f
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-03-03 02:03:16 +0300

    added TaskForm to Details part

--- src/components/Layout/Details/Details.js
+++ src/components/Layout/Details/Details.js
@@ -1,14 +1,22 @@
-import React from 'react'
-import { DetailsContainer, DetailsHeader, DetailsSectionOne, DetailsSectionTwo} from "./styles";
+import React from "react";
+import TaskForm from "../../Task/TaskForm/TaskForm";
+import {
+  DetailsContainer,
+  DetailsHeader,
+  DetailsSectionOne,
+  DetailsSectionTwo,
+} from "./styles";
 
 const Details = () => {
-    return (
-        <DetailsContainer>
-        <DetailsHeader></DetailsHeader>
-        <DetailsSectionOne></DetailsSectionOne>
-        <DetailsSectionTwo></DetailsSectionTwo>
-      </DetailsContainer>
-    )
-}
+  return (
+    <DetailsContainer>
+      <DetailsHeader></DetailsHeader>
+      <DetailsSectionOne>
+        <TaskForm />
+      </DetailsSectionOne>
+      <DetailsSectionTwo></DetailsSectionTwo>
+    </DetailsContainer>
+  );
+};
 
-export default Details
+export default Details;
