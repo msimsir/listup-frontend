1e3014a2ef6b7b8373a927e2b78c36bc0ef7c15b
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-03-02 22:09:09 +0300

    added contants

---
+++ src/constants/ui-elements.js
@@ -0,0 +1,15 @@
+export const searchPlaceholder = "Search task item, tag and list";
+
+export const sidebarMenuItems = [
+  "Today",
+  "Tomorrow",
+  "Next 7 Days",
+  "Trash",
+  "Routines",
+  "Done",
+  "Undone",
+  "Settings",
+];
+export const sidebarList = ["List", "Tag", "Schedule"];
+
+export const timeTags = ["Today", "Tomorrow", "Next 7 Days"];
