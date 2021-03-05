c63952b41469d7fb34058fde37e21c3cedceaae7
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-03-05 19:32:54 +0300

    updated Schedule component and styling

--- src/components/Schedule/SchedulePanel/SchedulePanel.js
+++ src/components/Schedule/SchedulePanel/SchedulePanel.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { IoAddCircleOutline } from "react-icons/io5";
+import LabelField from "../../UI/LabelField/LabelField";
+import Panel from "../../UI/Panel/Panel";
+import TextButton from "../../UI/TextButton/TextButton";
 
 const SchedulePanel = () => {
-    return (
-        <div>
-            SchedulePanel
-        </div>
-    )
-}
+  return (
+    <Panel>
+      <Panel height="200" overflow>
+        <LabelField padding size="small">
+          Schedule Panel
+        </LabelField>
+      </Panel>
+      <TextButton size="medium" margin="small">
+        <IoAddCircleOutline />
+        <span>Add Schedule</span>
+      </TextButton>
+    </Panel>
+  );
+};
 
-export default SchedulePanel
+export default SchedulePanel;
