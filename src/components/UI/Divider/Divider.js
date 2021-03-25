ac75bb7d78185e3fca9e6b99406baffbb3321b45
mehmetsimsir.1994 <mehmetsimsir.1994@gmail.com>
2021-03-25 21:44:46 +0300

    added Divider UI component

---
+++ src/components/UI/Divider/Divider.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+const Divider = styled.div`
+  border-bottom: 1px solid rgba(112, 112, 112, 0.4);
+  margin-top: 1rem;
+  margin-bottom: 0.5rem;
+`;
+
+export default Divider;
